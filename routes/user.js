0691d5954ee30729fd59d92307915ff3188ee19f
YashSharma80 <153749653+YashSharma80@users.noreply.github.com>
2026-05-04 10:14:09 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,32 +1,37 @@
-const express = require("express");
-const passport = require("passport");
-const User = require("../models/user");
+const express = require("express"); 
+const passport = require("passport"); 
+const User = require("../models/user"); 
 
-const router = express.Router();
+const router = express.Router(); 
 
+// show register page
 router.get("/register", (req, res) => {
   res.send("Register Page");
 });
 
+// create new user
 router.post("/register", async (req, res) => {
-  const user = new User(req.body);
-  await user.save();
+  const user = new User(req.body); 
+  await user.save(); 
   res.redirect("/login");
 });
 
+// show login page
 router.get("/login", (req, res) => {
   res.send("Login Page");
 });
 
+// login user using passport
 router.post("/login", passport.authenticate("local", {
-  successRedirect: "/recipes",
-  failureRedirect: "/login"
+  successRedirect: "/recipes", 
+  failureRedirect: "/login"    
 }));
 
+// logout user
 router.post("/logout", (req, res) => {
-  req.logout(() => {
-    res.redirect("/login");
+  req.logout(() => { 
+    res.redirect("/login"); 
   });
 });
-
-module.exports = router;
+// export router
+module.exports = router; 
